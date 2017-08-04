75f877605f14e6656e60bcff7abf7a43a972a1b2
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2017-08-04 09:37:35 -0400

    new day

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
+window.onload = function() {
+  console.log("DOM Ready!");
+  var frame;
+  var flying = setInterval(frame, 5);
+  var elem = document.getElementById("animate");
+  var position = 0;
 
-});
+  function flyingRocket() {
+    if (position == 350) {
+      clearInterval(flying);
+    } else {
+      position++;
+      elem.style.top = position + 'px';
+      elem.style.left = position + 'px';
+    }
+
+  }
+
+
+}; // end ready function
