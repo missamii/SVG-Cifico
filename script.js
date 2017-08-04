aa2b6a5e6d85f7ac61602150c08eeaa2153dace1
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2017-08-04 09:42:03 -0400

    var changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 window.onload = function() {
   console.log("DOM Ready!");
-  var frame;
-  var flying = setInterval(frame, 5);
-  var elem = document.getElementById("animate");
-  var position = 0;
+  var frame,
+    flying = setInterval(frame, 5),
+    elem = document.getElementById("animate"),
+    position = 0;
 
   function flyingRocket() {
     if (position == 350) {
       clearInterval(flying);
     } else {
       position++;
-      elem.style.top = position + 'px';
-      elem.style.left = position + 'px';
+      elem.style.top = position + 300;
+      elem.style.left = position + 300;
     }
 
   }
