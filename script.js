7970aafb0edb188418cc0ce08de145c771d1d844
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2017-08-07 12:48:00 -0400

    with values

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
+// I have a SVG with two circles that are animated using @keyframes. I want to display the value of the circle's growth. Using javascript I want to display the value as a they shrink and grow. getTotalLength() is probably not what I should be using but am not sure of the name of what I am looking for that does size/value check.
+// check for the correct JS method to solve this.
 window.onload = function() {
   console.log("DOM Ready!");
+  // get elements class names
+  var redCircle = document.getElementsByClassName('red')[0];
+  var greenCircle = document.getElementsByClassName('green')[0];
 
+  // get elements id for value, so the label spans I created
+  var redLabel = document.getElementById('red-grow');
+  var greenLabel = document.getElementById('green-grow');
 
-  // displaying each circle's value as they grow and shrink
-  var redValue = document.getElementById('red-grow').innerHTML = span;
-  // var greenValue = document.getElementById('green-grow').innerHTML =
+  function updateLabels() {
+    var transformValue = getComputedStyle(redCircle).getPropertyValue('transform');
+    getComputedStyle(greenCircle).getPropertyValue('transform');
 
-  var redCircle = document.getElementsByClassName('red');
-  var greenCircle = document.getElementsByClassName('green');
-  var current = 0;
-  var width = 0;
-  var destination = 700;
-  var friction = 0.04;
+    // transformValue a string | forming "matrix(1, 0, 0, 1, 0 ,0)"
+    var currentScale = transformValue.match(/matrix\(([\d.]+)\,/)[1];
 
-  // scaling up both circles
-  function scaleUp() {
-    //  console.log(scaleUp);
-    current += (destination - current) * friction;
-    redCircle[0].style.width = (current * 0.5 + 'px');
-    greenCircle[0].style.width = (current * 0.5 + 'px');
-    if (current >= destination - 0.1) {
-      // clearInterval(redAnimate);
-    }
+    redLabel.textContent = Math.round(currentScale * 133) + ': Red Circle Radius';
+    greenLabel.textContent = Math.round(currentScale * 133) + ': Green Circle Radius';
+    requestAnimationFrame(updateLabels);
   }
-  var redAnimate = setInterval(scaleUp, 20);
-
-  // scaling down both circles
-  function scaleDown() {
-    //console.log(redCircle[0].style, greenCircle[0].style);
-    current += (destination - current) * friction;
-    redCircle[0].style.width = (current * 0.5 + 'px');
-    greenCircle[0].style.width = (current * 0.5 + 'px');
-    if (current >= destination - 0.1) {
-      // clearInterval(greenAnimate);
-    }
-  }
-  var greenAnimate = setInterval(scaleDown, 20);
-}; // end ready function
+  requestAnimationFrame(updateLabels);
+};
